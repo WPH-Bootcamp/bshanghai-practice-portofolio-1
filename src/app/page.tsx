a92e0ac0ba5b7326235bc79fe965c45a4d8114cb
Henry Rivardo <henryrivardo07@gmail.com>
2025-10-20 21:43:41 +0700

    pertemuan 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import Hero from './home/partials/hero';
+import ImpactStatistics from './home/partials/impact-statistics';
 import Navbar from './home/partials/navbar';
+import ServicesProcess from './home/partials/services-process';
+import TrustedBy from './home/partials/trusted-by';
 
 export default function Home() {
   return (
     <div className=''>
       <Navbar />
       <Hero />
+      <TrustedBy />
+      <ImpactStatistics />
+      <ServicesProcess />
     </div>
   );
 }
